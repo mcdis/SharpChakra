1ea97012fcb5ac4d5411f991420bb2c3b61156d1
AlexanderSh <alexander.sholupov@gmail.com>
2017-08-19 15:15:54 +0300

    add extension GetProperty(string),HasProperty(string) in JsValueExtensions

--- src/SharpChakra.Extensions/JsValueExtensions.cs
+++ src/SharpChakra.Extensions/JsValueExtensions.cs
@@ -7,5 +7,9 @@
          _this.SetProperty(JsPropertyId.FromString(_propertyId), _value, _useStrictRules);
          return _this;
       }
+      public static JsValue GetProperty(this JsValue _this, string _propertyId)
+         => _this.GetProperty(JsPropertyId.FromString(_propertyId));
+      public static bool HasProperty(this JsValue _this, string _propertyId)
+         => _this.HasProperty(JsPropertyId.FromString(_propertyId));
    }
 }
