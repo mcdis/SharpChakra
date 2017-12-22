f25d9c18c299d62b5757b9b6af56c9f4272273cc
AlexanderSh <alexander.sholupov@gmail.com>
2017-12-22 18:04:52 +0300

    update ChackraCore dependency to 1.7.5

--- src/SharpChakra.Extensions/JsProxy.cs
+++ src/SharpChakra.Extensions/JsProxy.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Newtonsoft.Json.Linq;
 using SharpChakra.Json;
 
